31c91c930f5f1bd51ad0a789728ba2be5d14b297
makszimilien <nagy.tamas.kvk@gmail.com>
2023-04-28 17:03:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Styling
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector(".overlay").classList.add("hidden");
+  }, 1500); // 3000ms = 3 seconds
+});
