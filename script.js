5656a0bcd397220d2449924a929b4bf80b6bef97
makszimilien <nagy.tamas.kvk@gmail.com>
2023-04-29 18:43:50 +0200

    Add new empty page with arrows

--- script.js
+++ script.js
@@ -5,6 +5,19 @@
 window.addEventListener("load", function () {
   setTimeout(function () {
     document.querySelector(".overlay").classList.add("hidden");
-    console.log("hidden");
   }, 1800); // 3000ms = 3 seconds
 });
+
+// add even listener to element that has the class class="arrow-down" and when clicked, run the function
+// document
+//   .querySelectorAll(".arrow-down")
+//   .split(",")
+//   .map((element) =>
+//     element.addEventListener("click", function () {
+//       console.log(this.classList);
+//     })
+//   );
+// console.log(typeof document.querySelectorAll(".arrow-down"));
+// document.querySelectorAll(".arrow-down").map((element) => {
+//   console.log(element.parentElement);
+// });
